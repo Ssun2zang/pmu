05befb52cda0d6a7e33819a4a86b9e9a66c0d88d
Ssun2zang <mmmmhj@cau.ac.kr>
2022-11-09 19:32:14 +0900

    기반 다지는 중

--- main.py
+++ main.py
@@ -1,12 +1,75 @@
 import sys
 
-strings = []
-file = open(sys.argv[1], "r")
-while True:
-    line = file.readline()
-    if not line:
-        break
-    strings.append(line.strip())
-file.close()
+charClass = 0
+lexeme = ""
+nextChar = ""
+lexLen = 0
+token = 0
+nextToken = 0
 
-print(strings)
+LETTER = 0
+DIGIT = 1
+UNKNOWN = 99
+
+INT_LIT = 10
+IDENT = 11
+ASSIGN_OP = 20
+ADD_OP = 21
+SUB_OP = 22
+MULT_OP = 23
+DIV_OP = 24
+LEFT_PAREN = 25
+RIGHT_PAREN = 26
+EOP = -1
+
+def switch_func(x):
+    return {
+        '(' : LEFT_PAREN,
+        ')' : RIGHT_PAREN,
+        '+' : ADD_OP,
+        '-' : SUB_OP,
+        '*' : MULT_OP,
+        '/' : DIV_OP,
+    }.get(x, EOP)
+
+def lookup(ch):
+    addChar()
+    nextToken = switch_func(ch)
+    return nextToken
+
+def addChar():
+    # lexeme += nextChar
+    pass
+
+def getChar():
+    pass
+
+def getNonBlank():
+    pass
+
+def lexical():
+    pass
+
+
+def main():
+    strings = []
+    file = open(sys.argv[1], "r")
+    while True:
+        line = file.readline()
+        if not line:
+            break
+        strings.append(line.strip())
+    file.close()    
+    program = " ".join(strings)
+    print(program)
+    # print(lookup("+"))
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
